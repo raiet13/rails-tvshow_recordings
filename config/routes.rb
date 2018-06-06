dd763f81c400d1fb679bcbffe12e11908b4e6515
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 21:46:13 +0000

    updated characters to be nested resource to shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :shows
+  
+  resources :shows do
+    resources :characters
+  end
+
   resources :recordings
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
