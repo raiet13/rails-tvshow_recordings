ea7ab782198ee39dd3e69e63fe4149784dd2c6a9
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 21:43:11 +0000

    add recording resource

---
+++ db/migrate/20180606214152_create_recordings.rb
@@ -0,0 +1,10 @@
+class CreateRecordings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recordings do |t|
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :show, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
