0aa6d4db622bfaefca6c50b5d4dece35698b8c6f
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 22:26:59 +0000

    updated route file organization for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
 
   root 'application#home'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 
   resources :shows do
     resources :characters
   end
 
   resources :recordings
-  resources :users, only: [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
