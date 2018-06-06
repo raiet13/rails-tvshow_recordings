d55b275950a26b312bfaf8947e5ef55fa7c2a91e
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 22:26:13 +0000

    updated user table with username + updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,31 @@ class SessionsController < ApplicationController
 
   # Log In Page
   def new
+    if logged_in?
+      redirect_to user_path(current_user)
+    end
+    @users = User.all
   end
 
   # Log In Authentication
   def create
+    # puts "Sign In Params = #{params}"
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:notice] = "Log in unsuccessful, either your username or password was not found. Please try again or sign up for an account."
+      redirect_to login_path
+    end
   end
 
   # Log Out
   def destroy
+    if session[:user_id]
+      session.clear
+    end
+    redirect_to root_path
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_06_06_220249) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "username"
     t.string "password_digest"
     t.integer "age"
     t.integer "recording_hours"
