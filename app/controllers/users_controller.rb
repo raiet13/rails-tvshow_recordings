7adaa628e29e551d4a581a00d8e465ff80fa6b69
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 23:01:16 +0000

    added framework for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,48 @@
 class UsersController < ApplicationController
   before_action :redirect_if_not_logged_in
+
+  # Show All Users
+  def index
+  # Show All By (methods in user model)
+    # User's name
+    # User's (current) active recordings
+    # ??? -- User's total num recordings to date
+    # User's favorite show
+    # User's favorite genre
+  end
+
+  # Sign Up Page
+  def new
+  end
+
+  # User Creation/Data Authentication
+  def create
+  end
+
+  # User Profile
+  def show
+    # User name
+    # User's (current) active recordings
+    # ??? -- User's total num recordings to date
+    # User's favorite show
+    # User's favorite genre
+    # Link to "See All Shows" (will have recorded shows auto-checked + button to "record show?" if not)
+    # Link to "See Your Recordings"
+  end
+
+  # Edit User Info?
+  def edit
+    # maybe only show username for modification? -- TBD
+  end
+
+  # Update User Info?
+  def update
+    # NOTE : Same as above -- maybe just do user edit info as delete path on show page and then through the recordings model (allow recording/deactive/etc.)
+  end
+
+  # Delete User
+  def destroy
+  end
+
+
 end
