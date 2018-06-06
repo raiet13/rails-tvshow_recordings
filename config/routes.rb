f01c75ff1753b5c81dff5cda483681142abdd841
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 22:17:04 +0000

    added login route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   root 'application#home'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
 
   resources :shows do
     resources :characters
