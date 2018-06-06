e545658b38e47ad3fd1aa62c7747b6f3c772fda6
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 22:08:29 +0000

    added basic AR to show model

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,7 @@
 class Show < ApplicationRecord
+  has_many :recordings
+  has_many :users, through: :recordings
+  has_many :characters
+  # NOTE : MABYE For additional complexity later -- has_many show_genres | has_many genres through show_genres
+
 end
