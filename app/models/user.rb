f3a6df653382ba488d74df03e93922914c8fcfe2
Raiet Intraub <raietsaeed@gmail.com>
2018-06-06 21:50:10 +0000

    updated user model with AR basics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  # NOTE : INSERT VALIDATIONS here
+  has_many: :recordings
+  has_many: :shows, through: :recordings
+
+
+  # NOTE : Add checks for recordings (show is age appropriate / has enough recording hours)
+
 end
