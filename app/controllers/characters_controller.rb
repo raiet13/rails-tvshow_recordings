01a0a8fb38f252e47fa5feb8cc225401880534e3
Raiet Intraub <raietsaeed@gmail.com>
2018-06-08 02:04:08 +0000

    created characters controller

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,25 @@
+class CharactersController < ApplicationController
+  before_action :redirect_if_not_logged_in
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
