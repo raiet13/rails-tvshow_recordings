26316375852773e6358e59dad6059e9656f329f6
Raiet Intraub <raietsaeed@gmail.com>
2018-06-08 00:27:51 +0000

    added model validations

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,12 @@
 class Show < ApplicationRecord
+  validates_presence_of :name, :req_recording_hours, :req_age
+
   has_many :recordings
   has_many :users, through: :recordings
   has_many :characters
-  # NOTE : MABYE For additional complexity later -- has_many show_genres | has_many genres through show_genres
+
+  # NOTE : MAYBE For additional complexity later -- has_many show_genres | has_many genres through show_genres
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
-  # NOTE : INSERT VALIDATIONS here
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+
   has_many :recordings
   has_many :shows, through: :recordings
 
 
   # NOTE : Add checks for recordings (show is age appropriate / has enough recording hours)
+  # NOTE : Need to confirm that age and recording hours is not null (report that recordings require reported age/recording hours to be valid)
+    # t.integer :age
+    # t.integer :recording_hours
+
 
 end
