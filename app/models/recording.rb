69e01584edaa6d6f5f0a1163c00ca5872391955a
Raiet Intraub <raietsaeed@gmail.com>
2018-06-09 21:09:16 +0000

    added frame for recording model methods

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -8,4 +8,28 @@ class Recording < ApplicationRecord
   # NOTE : Added "active" flag for active vs deleted recordings + method for retrieving each type <<-- Added to User model
   # NOTE : Added name for user submittable attribute
 
+
+
+  ## Methods for allowing recording or not ##
+
+  # Main method for allowing recording to be saved and making user updates
+  def allow_recording
+  end
+
+  # Main method for checking
+  def check_recording
+  end
+
+  # Check user has enough hours to record show
+  def check_hours
+  end
+
+  # Check user is old enough to record show
+  def check_age
+  end
+
+  # Method for updating user current hours
+  def update_user_hours
+  end
+
 end
