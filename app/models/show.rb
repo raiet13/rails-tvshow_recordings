7a4d07cfa8796647e034111fef451e6302bd55a2
Raiet Intraub <raietsaeed@gmail.com>
2018-06-10 21:29:04 +0000

    added show class scope level method

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,6 +7,22 @@ class Show < ApplicationRecord
   has_many :characters
   # NOTE : MAYBE For additional complexity later -- has_many show_genres | has_many genres through show_genres
 
+  # - Show.most_recorded (top 5 recorded shows? or just order by it while showing num_recorders)
+  # -> reference captains/boats/categories lab --> Group by then order by
 
+  # Class scope method for requirement?
+  def self.most_recorded
+    # TaJ (3) -> OUAT/S (2)
+    num_recordings = {}
+    Show.all.collect do |show|
+      num_recordings[show.name] = show.num_recordings
+    end
+    sorted = num_recordings.sort_by{ |show, rec_count| rec_count }.reverse
+    # sorted.first
+  end
+
+  def num_recordings
+    self.recordings.count
+  end
 
 end
