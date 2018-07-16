7bfbd844ee5389cd3d675658edbba9cf8e2db696
Raiet <raietsaeed@gmail.com>
2018-07-16 00:36:55 +0000

    updated functions for character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,8 +3,22 @@ class Character < ApplicationRecord
   validates_presence_of :name
   validates_associated :show
 
+  def self.most_viewed_by_show(char_show)
+    chars = self.select(:name, :char_page_views).where(show: char_show)
+    chars.select(:name, :char_page_views).order(char_page_views: :desc).first
+  end
+  
+  def self.by_views_by_show(char_show)
+    chars = self.select(:name, :char_page_views).where(show: char_show)
+    chars.select(:name, :char_page_views).order(char_page_views: :desc)
+  end
+
   def self.most_viewed
-    self.select(:name, :char_page_views).order(char_page_views: :asc)
+    chars.select(:name, :char_page_views).order(char_page_views: :desc).first
+  end
+
+  def self.by_views
+    self.select(:name, :char_page_views).order(char_page_views: :desc)
   end
 
 end
