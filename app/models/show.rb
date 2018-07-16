2d30bd462393e037ee40673143e9e13e834da006
Raiet <raietsaeed@gmail.com>
2018-07-16 03:35:33 +0000

    final updates while filling out form

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,19 +5,6 @@ class Show < ApplicationRecord
   has_many :recordings
   has_many :users, through: :recordings
   has_many :characters
-  # NOTE : MAYBE For additional complexity later -- has_many show_genres | has_many genres through show_genres
-
-
-  # Class scope method for requirement?
-  def self.most_recorded
-    # TaJ (3) -> OUAT/S (2)
-    num_recordings = {}
-    Show.all.collect do |show|
-      num_recordings[show.name] = show.num_recordings
-    end
-    sorted = num_recordings.sort_by{ |show, rec_count| rec_count }.reverse
-    # sorted.first
-  end
 
   def num_recordings
     self.recordings.count
